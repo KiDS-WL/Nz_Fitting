be26a7d0befe9c78963735f70ba8a0b3cb1427ff
jlvdb <jlvdb@astro.uni-bonn.de>
2020-02-20 12:51:28 +0100

    disable numpy threading

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+import os
+# disable threading in numpy ONLY IF it has not been imported yet
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1'
+
 from .data import RedshiftData, RedshiftDataBinned, RedshiftHistogram, RedshiftHistogramBinned
 from .fitting import CurveFit, FitResult
 from .models import (BiasFitModel, CombModelBinned, GaussianComb,
