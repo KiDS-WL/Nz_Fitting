221bb4d4be7c0117a3f032151c9b404a5bbe69ef
jlvdb <jlvdb@astro.uni-bonn.de>
2020-02-20 12:55:10 +0100

    evaluate returns RedshiftData*, removed plotting, bug fixes

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from scipy.special import erfinv
 from scipy.interpolate import interp1d
 
-from .data import RedshiftData
+from .data import RedshiftData, RedshiftDataBinned
 from .utils import Figure
 
 
@@ -60,6 +60,11 @@ class BaseModel(object):
     def getParamGuess(self):
         return [p.guess for p in self._paramlist]
 
+    def setParamGuess(self, guesses):
+        assert(len(guesses) == len(self._paramlist))
+        for param, guess in zip(self._paramlist, guesses):
+            param.setGuess(guess)
+
     def getParamBounds(self, pairwise=False):
         if pairwise:
             return [(p.lower, p.upper) for p in self._paramlist]
@@ -91,14 +96,14 @@ class BaseModel(object):
         except ValueError:
             return self(data, *params)
 
-    def _computePlot(self, z_data, params):
+    def _evalZ(self, z_data):
         # get the data needed to evaluate the model and make the plot
         if hasattr(z_data, "centers"):
-            z_plot = z_data.centers()
+            z = z_data.centers()
         elif hasattr(z_data, "z"):
-            z_plot = z_data.z(all=(self.expect_z == "edges"))
+            z = z_data.z(all=(self.expect_z == "edges"))
         else:
-            z_plot = copy(z_data)
+            z = copy(z_data)
         if self.expect_z == "edges":
             if hasattr(z_data, "edges"):
                 z_model = z_data.edges()
@@ -106,35 +111,32 @@ class BaseModel(object):
                 # the input are bin edges so we must compute the centers
                 z_model = copy(z_data)
                 try:
-                    z_plot = (z_model[1:] + z_model[:-1]) / 2.0
+                    z = (z_model[1:] + z_model[:-1]) / 2.0
                 except TypeError:
-                    z_plot = [(z[1:] + z[:-1]) / 2.0 for z in z_model]
+                    z = [(edges[1:] + edges[:-1]) / 2.0 for edges in z_model]
         else:
-            z_model = z_plot
+            z_model = z
+        return z, z_model
+
+    def evaluate(self, z_data, params):
+        z, z_model = self._evalZ(z_data)
         # call the model
         try:
-            iter(params)
-        except TypeError as e:  # parameters are a FitResult instance
-            NotImplemented
-        else:
-            data = self(z_model, *params)
-            errors = None
-        return z_plot, data, errors
-
-    def plot(self, z_data, params, ax=None, **kwargs):
-        if ax is None:
-            fig = Figure(1)
-            ax = fig.axes[0]
-        else:
-            fig = plt.gcf()
-        # make the plot
-        z, data, error = self._computePlot(z_data, params)
-        line = ax.plot(z, data, **kwargs)[0]
-        if error is not None:
-            ax.fill_between(
-                z, data - error, data + error,
-                alpha=0.3, color=line.get_color())
-        return fig
+            param_samples = params.paramSamples()
+            n = self(z_model, *params.paramBest())
+            samples = np.empty((len(param_samples), len(n)))
+            for i, param in enumerate(param_samples):
+                samples[i] = self(z_model, *param)
+            dn = samples.std(axis=0)
+        except AttributeError:
+            n = self(z_model, *params)
+            samples = None
+            dn = np.zeros_like(n)
+        # pack the data as a RedshiftData container
+        container = RedshiftData(z, n, dn)
+        if samples is not None:
+            container.setSamples(samples)
+        return container
 
 
 class BaseModelBinned(BaseModel):
@@ -151,17 +153,31 @@ class BaseModelBinned(BaseModel):
             axes = np.asarray(fig.axes)
         return fig, axes
 
-    def plot(self, z_data, params, fig=None, **kwargs):
-        fig, axes = self._getFig(fig)
-        # make the plot
-        z, data, error = self._computePlot(z_data, params)
-        for i, ax in enumerate(axes.flatten()):
-            line = ax.plot(z[i], data[i], **kwargs)[0]
-            if error is not None:
-                ax.fill_between(
-                    z[i], data[i] - error[i], data[i] + error[i],
-                    alpha=0.3, color=line.get_color())
-        return fig
+    def evaluate(self, z_data, params):
+        z, z_model = self._evalZ(z_data)
+        # call the model
+        try:
+            param_samples = params.paramSamples()
+            n = self(z_model, *params.paramBest())
+            samples = [
+                np.empty((len(param_samples), len(bin_n))) for bin_n in n]
+            for i, param in enumerate(param_samples):
+                for j, bin_sample in enumerate(self(z_model, *param)):
+                    samples[j][i] = bin_sample
+            dn = [bin_sample.std(axis=0) for bin_sample in samples]
+        except AttributeError:
+            n = self(z_model, *params)
+            samples = None
+            dn = [np.zeros_like(bin_n) for bin_n in n]
+        # pack the data as a RedshiftDataBinned container
+        bins = []
+        for i in range(len(n)):
+            container = RedshiftData(z[i], n[i], dn[i])
+            if samples is not None:
+                container.setSamples(samples[i])
+            bins.append(container)
+        container = RedshiftDataBinned(bins[:-1], bins[-1])
+        return container
 
 
 class PowerLawBias(BaseModel):
@@ -229,6 +245,9 @@ class ShiftModel(BaseModel):
         cdf_shifted /= cdf_shifted[-1]  # normalize
         # compute the new PDF
         pdf_shifted = np.diff(cdf_shifted) / np.diff(edges_shifted)
+        # mask out model values where the data is not defined
+        if hasattr(z_data, "mask"):
+            pdf_shifted = pdf_shifted[~z_data.mask()]
         return pdf_shifted * params[1]  # rescale to the data
 
 
@@ -256,8 +275,9 @@ class ShiftModelBinned(BaseModelBinned):
             self._paramlist.extend(bias_model.getParams())
             self._param_per_model.append(bias_model.getParamNo())
 
-    def __call__(self, z, *params):
-        assert(len(z) == len(self._models))
+    def __call__(self, z_data, *params):
+        assert(len(z_data) == len(self._models))
+        z = self.getZ(z_data)
         # split parameters, last array is empty or holds bias model params
         param_tuples = np.split(params, np.cumsum(self._param_per_model))
         # evaluate each model
@@ -267,6 +287,10 @@ class ShiftModelBinned(BaseModelBinned):
             if self._bias_model is not None:  # multiply with bias to mach data
                 pdf *= self._bias(bin_z, *param_tuples[-1])
             bin_pdfs.append(pdf)
+        # mask out model values where the data is not defined
+        if hasattr(z_data, "getData"):
+            for i, data in enumerate(z_data.getData()):
+                bin_pdfs[i] = bin_pdfs[i][~data.mask()]
         return bin_pdfs
 
 
@@ -329,8 +353,9 @@ class GaussianComb(CombModel):
             param.setGuess(1.0)
             param.setBounds(lower=0.0)
 
-    def __call__(self, z, *params):
+    def __call__(self, z_data, *params):
         self._checkParamNo(params)
+        z = self.getZ(z_data)
         if len(params) != len(self._paramlist):
             raise ValueError(
                 "expected {:d} parameters but got {:d}".format(
@@ -349,8 +374,9 @@ class LogGaussianComb(CombModel):
         for param in self._paramlist:
             param.setGuess(0.0)
 
-    def __call__(self, z, *params):
+    def __call__(self, z_data, *params):
         self._checkParamNo(params)
+        z = self.getZ(z_data)
         n_z = z * np.sum([
             np.exp(amp) * self.gaussian(z, mu, sig)
             for amp, mu, sig in
@@ -384,11 +410,12 @@ class CombModelBinned(BaseModelBinned):
             self._paramlist.extend(bias_model.getParams())
             self._param_per_model.append(bias_model.getParamNo())
 
-    def __call__(self, z, *params):
-        if len(z) != (len(self._models) + 1):
+    def __call__(self, z_data, *params):
+        if len(z_data) != (len(self._models) + 1):
             raise ValueError(
                 "expected {:d} redshift samplings for {:} bins".format(
                     len(self._models) + 1, len(self._models)))
+        z = self.getZ(z_data)
         # split parameters, last array is empty or holds bias model params
         param_tuples = np.split(params, np.cumsum(self._param_per_model))
         # evaluate each model
